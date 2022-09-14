1aa35efe8276d9d788289640ce01f7cdfcfcce2d
Ashe546 <asfyd42@gmail.com>
2022-09-14 05:45:43 -0300

    Added some more testes for the calculations

--- src/__tests__/calculate.test.jsx
+++ src/__tests__/calculate.test.jsx
@@ -25,3 +25,48 @@ it('check if returned obj is empty, when total contains period', () => {
 
   expect(calculate(obj, btnPressed)).toEqual({});
 });
+
+it('check if returned obj is empty, when AC is pressed', () => {
+  const btnPressed = 'AC';
+  const obj = {
+    total: '10',
+    next: '0',
+    operation: '+',
+  };
+
+  expect(calculate(obj, btnPressed)).toEqual({
+    total: null,
+    next: null,
+    operation: null,
+  });
+});
+
+it('check if returned obj is +/-, when +/- is pressed', () => {
+  const btnPressed = '+/-';
+  const obj = {
+    total: '10',
+    next: null,
+    operation: '+/-',
+  };
+
+  expect(calculate(obj, btnPressed)).toEqual({
+    total: '-10',
+    next: null,
+    operation: '+/-',
+  });
+});
+
+it('check if returned obj is 1, when % is pressed', () => {
+  const btnPressed = '%';
+  const obj = {
+    total: '5',
+    next: '2',
+    operation: '%',
+  };
+
+  expect(calculate(obj, btnPressed)).toEqual({
+    total: '1',
+    next: null,
+    operation: '%',
+  });
+});
